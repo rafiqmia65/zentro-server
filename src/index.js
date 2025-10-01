e226da425425f45d0982781a20163a7d60770421
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-10-02 00:15:59 +0600

    feat: testing purpose of -u

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ connectDb();
 app.listen(port, () => {
   console.log(`🚀 Zentro Place app listening on port ${port}`);
   console.log('This is for development purpose .');
+  console.log('This is for development purpose 2 .');
 
 });
 
