7960129b9fcc524c5ac32475b79814c41d5e094d
Abdul Al Roman <mdroman45678910@gmail.com>
2025-10-03 19:06:10 +0600

    feat(routers): add Agent to server.js to agentRoutes.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import propertyRoutes from "./routes/propertyRoutes.js";
+import AgentRoutes from "./routes/agentRoutes.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 // Routes
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/property", propertyRoutes);
+app.use("/api/v1/agent", AgentRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Zentro Place Server is Cooking!!" });
