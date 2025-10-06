919614142d4e4a0656320560a68a1ce57f34d917
Abdul Al Roman <mdroman45678910@gmail.com>
2025-10-06 23:16:11 +0600

    feat(blog): add blog in router and controller page

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -9,10 +9,19 @@ import {
 
 const router = express.Router();
 
+// Route to create a new blog
 router.post("/add-blog/:authorId", createBlog);
+
+// Route to get all blogs
 router.get("/get-all-blog", getAllBlog);
+
+// Route to get a single blog by its ID
 router.get("/get-single-blog/:blogId", getSingleBlog);
+
+// Route to update a blog by its ID
 router.put("/update-blog/:blogId", updateBlog);
+
+// Route to delete a blog by its ID
 router.delete("/delete-blog/:blogId", deleteBlog);
 
 export default router;
